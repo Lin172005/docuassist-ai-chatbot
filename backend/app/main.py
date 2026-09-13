1f4c87ad67de8acd82890bd0b2d47550e75d932a
Praiselin <praiselin2005@gmail.com>
2026-09-13 19:59:46 +0530

    updated main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -17,8 +17,7 @@ allowed_origins = [
     origin.strip()
     for origin in os.getenv(
         "ALLOWED_ORIGINS",
-        "http://localhost:3000",
-        "https://docuassist-ai-chatbot-nine.vercel.app",
+        "http://localhost:3000","https://docuassist-ai-chatbot-nine.vercel.app",
     ).split(",")
     if origin.strip()
 ]
